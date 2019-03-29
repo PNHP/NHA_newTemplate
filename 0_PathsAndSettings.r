871d60873ee17ab853320073963179aebda7f3f5
Christopher Tracey <ctracey@paconserve.org>
2019-03-29 15:28:12 -0400

    add NHA path

--- 0_PathsAndSettings.r
+++ 0_PathsAndSettings.r
@@ -6,6 +6,8 @@ arc.check_product()
 ## Biotics Geodatabase
 biotics_gdb <- "W:/Heritage/Heritage_Data/Biotics_datasets.gdb"
 
+# NHA Databases and such
+NHA_path <- "P:/Conservation Programs/Natural Heritage Program/ConservationPlanning/NHA_ToolsV3"
 
 # output database name
 ####databasename <- here::here("_data","output","coa_bridgetest.sqlite")
