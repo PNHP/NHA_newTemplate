a80275462a20ce198bdc57cf8a4aeb4250e6dde0
Anna-Johnson <44846295+Anna-Johnson@users.noreply.github.com>
2019-05-15 15:41:10 -0400

    updates to database creation

--- scripts/nha_ThreatsRecDatabase/1_ETimporter.r
+++ scripts/nha_ThreatsRecDatabase/1_ETimporter.r
@@ -21,6 +21,8 @@ require(RSQLite)
 databasename <- "nha_recs.sqlite" 
 databasename <- here("_data","databases",databasename)
 
+#Import current Element Tracking (ET) file into NHA database
+
 ET_path <- "P://Conservation Programs/Natural Heritage Program/Data Management/Biotics Database Areas/Element Tracking/current element lists"
 
 #get the threats template
@@ -39,14 +41,34 @@ rm(n)
 # subset to tracked species
 ET <- ET[which(ET$TRACKING.STATUS=="Y"|ET$TRACKING.STATUS=="W"),]
 
-# rename colums here
+# rename columns to match geodatabase names, where overlap occurs 
 
-# fix dates openxlsx has command
+names(ET) <- c("Element.Subnational.ID","ELCODE","SNAME","SCOMNAME","G_RANK","S_RANK","SRANK.CHANGE.DATE","SRANK.REVIEW.DATE","TRACKING.STATUS","PA.FED.STATUS","S_PROTECTI","PBSSTATUS","PBS.DATE","PBS.QUALIFIER","SGCN.STATUS","SGCN.COMMENTS","SENSITIVE_","AQUATIC.INDICATOR","ER.RULE")
+
+# change dates from excel format to rest of the world format (assuming we are working w/ Excel 2010)
+ET$SRANK.CHANGE.DATE <- convertToDate(ET$SRANK.CHANGE.DATE, origin="1900-01-01")
+ET$SRANK.REVIEW.DATE <- convertToDate(ET$SRANK.REVIEW.DATE, origin="1900-01-01")
+ET$PBS.DATE <- convertToDate(ET$PBS.DATE, origin="1900-01-01")
 
 #write to database
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
 dbWriteTable(db, "ET", ET, overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
 
+
+
+#Import current threats/recs tables
+
+ThreatRecPath <- "P://Conservation Programs/Natural Heritage Program/ConservationPlanning/NaturalHeritageAreas/NHA_Tool/Copy of ELCODE_threatsrecs_database_allEOs_elsubid.xlsx"  
+
+ThreatRecTable <- read.xlsx(xlsxFile=ThreatRecPath, sheet="ThreatRecTable", skipEmptyRows=FALSE, rowNames=FALSE)
+
+ThreatRecTable <- read.xlsx(xlsxFile=ThreatRecPath, sheet="ThreatRecTable", skipEmptyRows=FALSE, rowNames=FALSE)
+
+
+#write to database
+db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
+dbWriteTable(db, "ThreatRecTable", ThreatRecTable, overwrite=TRUE) # write the table to the sqlite
+dbDisconnect(db) # disconnect the db
 
 
