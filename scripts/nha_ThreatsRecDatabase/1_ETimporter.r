e69e42ac06aea9ed8cfdf8962afc46b8d6e33701
Anna-Johnson <44846295+Anna-Johnson@users.noreply.github.com>
2019-05-16 12:36:06 -0400

    Update 1_ETimporter.r

--- scripts/nha_ThreatsRecDatabase/1_ETimporter.r
+++ scripts/nha_ThreatsRecDatabase/1_ETimporter.r
@@ -23,15 +23,14 @@ require(RSQLite)
 # Set input paths ----
 databasename <- "nha_recs.sqlite" 
 databasename <- here("_data","databases",databasename)
-ET_path <- "P://Conservation Programs/Natural Heritage Program/Data Management/Biotics Database Areas/Element Tracking/current element lists"
-# this is the path to the element tracking list folder on the p-drive in Pittsburgh.
 
 #Import current Element Tracking (ET) file into NHA database
 
 ET_path <- "P://Conservation Programs/Natural Heritage Program/Data Management/Biotics Database Areas/Element Tracking/current element lists"
 
-#get the threats template
+# this is the path to the element tracking list folder on the p-drive in Pittsburgh.
 
+# get the threats template
 ET_file <- list.files(path=ET_path, pattern=".xlsx$")  # --- make sure your excel file is not open.
 ET_file
 # look at the output and choose which shapefile you want to run
@@ -51,6 +50,12 @@ ET <- ET[which(ET$TRACKING.STATUS=="Y"|ET$TRACKING.STATUS=="W"),]
 
 names(ET) <- c("Element.Subnational.ID","ELCODE","SNAME","SCOMNAME","G_RANK","S_RANK","SRANK.CHANGE.DATE","SRANK.REVIEW.DATE","TRACKING.STATUS","PA.FED.STATUS","S_PROTECTI","PBSSTATUS","PBS.DATE","PBS.QUALIFIER","SGCN.STATUS","SGCN.COMMENTS","SENSITIVE_","AQUATIC.INDICATOR","ER.RULE")
 
+names(ET)[names(ET) == "SCIENTIFIC.NAME"] <- "SNAME"
+names(ET)[names(ET) == "COMMON.NAME"] <- "SCOMNAME"
+names(ET)[names(ET) == "G.RANK"] <- "G_RANK"
+names(ET)[names(ET) == "S.RANK"] <- "S_RANK"
+names(ET)[names(ET) == "SENSITIVE.SPECIES"] <- "SENSITIVE_"
+
 # change dates from excel format to rest of the world format (assuming we are working w/ Excel 2010)
 ET$SRANK.CHANGE.DATE <- convertToDate(ET$SRANK.CHANGE.DATE, origin="1900-01-01")
 ET$SRANK.REVIEW.DATE <- convertToDate(ET$SRANK.REVIEW.DATE, origin="1900-01-01")
@@ -67,9 +72,17 @@ ThreatRecPath <- "P://Conservation Programs/Natural Heritage Program/Conservatio
 
 ThreatRecTable <- read.xlsx(xlsxFile=ThreatRecPath, sheet="ThreatRecTable", skipEmptyRows=FALSE, rowNames=FALSE)
 
-ThreatRecTable <- read.xlsx(xlsxFile=ThreatRecPath, sheet="ThreatRecTable", skipEmptyRows=FALSE, rowNames=FALSE)
-
 #write to database
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
 dbWriteTable(db, "ThreatRecTable", ThreatRecTable, overwrite=TRUE) # write the table to the sqlite
+dbDisconnect(db) # disconnect the db
+
+#Import 
+
+ElementThreatRecs <- read.xlsx(xlsxFile=ThreatRecPath, sheet="ALLEOs", skipEmptyRows=FALSE, rowNames=FALSE)
+names(ElementThreatRecs)
+
+#write to database
+db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
+dbWriteTable(db, "ElementThreatRecs", ElementThreatRecs[,c(2:5,8)], overwrite=TRUE) # write the table to the sqlite
 dbDisconnect(db) # disconnect the db
