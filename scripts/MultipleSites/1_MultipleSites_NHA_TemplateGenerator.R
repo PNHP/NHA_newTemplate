17264d450bb68b1885d930f1341e08547d12b1fd
Anna-Johnson <44846295+Anna-Johnson@users.noreply.github.com>
2019-10-18 13:47:56 -0400

    more updates to multiple sites script

--- scripts/MultipleSites/1_MultipleSites_NHA_TemplateGenerator.R
+++ scripts/MultipleSites/1_MultipleSites_NHA_TemplateGenerator.R
@@ -102,14 +102,15 @@ for (i in 1:length(Site_Name_List)) {
 
 species_table_select #list of species tables
 
-removecols <- c("OBJECTID","REVIEWER","REVIEW_STATUS","NHA_JOIN_ID","GlobalID")
-for (name in removecols) { 
-    species_table_select[[name]] <- NULL
-   }
+#removecols <- c("OBJECTID","REVIEWER","REVIEW_STATUS","NHA_JOIN_ID","GlobalID")
+#for (name in removecols) { 
+ #   species_table_select[[name]] <- NULL
+  # }
+
 SD_speciesTable <- lapply(seq_along(species_table_select),
-                             function(x) species_table_select[[x]][,c("EO_ID","ELCODE","SNAME","SCOMNAME","ELEMENT_TYPE","G_RANK","S_RANK","S_PROTECTI","PBSSTATUS","LAST_OBS_D","BASIC_EO_R","SENSITIVE_")])
+                             function(x) species_table_select[[x]][,c("EO_ID","ELCODE","SNAME","SCOMNAME","ELEMENT_TYPE","NHA_JOIN_ID", "G_RANK","S_RANK","S_PROTECTI","PBSSTATUS","LAST_OBS_D","BASIC_EO_R","SENSITIVE_")])
   
-stable_names <- c("EO_ID","ELCODE","SNAME","SCOMNAME","ELEMENT_TYPE","GRANK","SRANK","SPROT","PBSSTATUS","LASTOBS","EORANK","SENSITIVE")
+stable_names <- c("EO_ID","ELCODE","SNAME","SCOMNAME","ELEMENT_TYPE","NHA_JOIN_ID","GRANK","SRANK","SPROT","PBSSTATUS","LASTOBS","EORANK","SENSITIVE")
 
 SD_speciesTable <- lapply(SD_speciesTable, setNames, stable_names) #List of species tables for each selected NHA
 SD_specieslist <- lapply(seq_along(SD_speciesTable ),
@@ -131,39 +132,129 @@ for (i in 1:length(SD_speciesTable)) {
     }
 }
 
-# modify image assignments to account for finer groupings of the inverts
-for (i in 1:length(SD_speciesTable)) {
-  for(j in 1:nrow(SD_speciesTable[[i]])){
-    SD_speciesTable[[i]]$Images <- EO_ImFix(SD_speciesTable[[i]][j,])
-  }
-}
+# modify image assignments to account for finer groupings of the inverts--this part is not working right, come back to later
+#for (i in 1:length(SD_speciesTable)) {
+#  for(j in 1:nrow(SD_speciesTable[[i]])){
+#    SD_speciesTable[[i]]$Images <- EO_ImFix(SD_speciesTable[[i]][j,])
+#  }
+#}
 
 # write this table to the SQLite database
 speciesTable4db <- SD_speciesTable
 
-#
-#
-#
-#
-#
-#
-###########################
-####from here not working##
-############################
-selected_nhasl <- for (i in 1:length(selected_nhas)){
-  selected_nhas[[i]] <- as.data.frame(selected_nhas[[i]])
-} 
 for (i in 1:length(speciesTable4db)){
-  speciesTable4db[[i]] <- cbind(selected_nhas[i]$NHA_JOIN_ID, speciesTable4db[[i]])
+  speciesTable4db[[i]] <- cbind(selected_nhas$NHA_JOIN_ID[i], speciesTable4db[[i]])
 }
 
-speciesTable4db <- cbind(selected_nhas$NHA_JOIN_ID, speciesTable4db)
-colnames(speciesTable4db)[which(names(speciesTable4db) == "selected_nha$NHA_JOIN_ID")] <- "NHA_JOIN_ID"
-speciesTable4db$NHA_JOIN_ID <- as.character(speciesTable4db$NHA_JOIN_ID)
+for (i in 1:length(speciesTable4db)){
+ names(speciesTable4db[[i]])[1] <- "NHA_JOIN_ID"
+ speciesTable4db[[i]]$NHA_JOIN_ID <- as.character(speciesTable4db[[i]]$NHA_JOIN_ID) 
+}
 
 db_nha <- dbConnect(SQLite(), dbname=nha_databasename) # connect to the database
 # delete existing threats and recs for this site if they exist
-dbExecute(db_nha, paste("DELETE FROM nha_species WHERE NHA_JOIN_ID = ", sQuote(selected_nha$NHA_JOIN_ID), sep=""))
+for (i in 1:length(selected_nhas$NHA_JOIN_ID)){
+  dbExecute(db_nha, paste("DELETE FROM nha_species WHERE NHA_JOIN_ID = ", sQuote(selected_nhas$NHA_JOIN_ID[i]), sep=""))
+}
+
 # add in the new data
-dbAppendTable(db_nha, "nha_species", speciesTable4db)
+for (i in 1:length(speciesTable4db)){  
+dbAppendTable(db_nha, "nha_species", speciesTable4db[[i]])
+}
 dbDisconnect(db_nha)
+
+#check whether there are multiple EOs in the species table for the same species, and only keep one record for each species, the most recently observed entry
+for (i in 1:length(SD_speciesTable)) {
+duplic_Spp <- SD_speciesTable[[i]] %>%
+  group_by(ELCODE) %>%
+  mutate(dupe=n()>1)
+duplic_Spp <- as.data.frame(duplic_Spp)
+duplic_Spp2 <- duplic_Spp[(duplic_Spp$dupe != TRUE),] #remove rows which are the same species
+same <- duplic_Spp[(duplic_Spp$dupe == TRUE),]#select the rows which are the same
+same <- same[order(same$LASTOBS, decreasing=TRUE),] #sort by date, so that when unique is called, the most recent rows are preserved
+unique <- unique(same[,-15])#remove the duplication code column
+SD_speciesTable[[i]] <- as.data.frame(rbind(SD_speciesTable[[i]], unique))
+}
+
+
+### Pull out info from Biotics for each site ###
+
+eoid_list <- list() #list of EOIDs to pull Biotics records with
+
+
+for (i in 1: length(SD_speciesTable)){
+eoid_list[[i]] <- paste(toString(SD_speciesTable[[i]]$EO_ID), collapse = ",") # make a list of EOIDs to get data from
+}
+
+ptreps <- arc.open(paste(biotics_gdb,"eo_ptreps",sep="/"))
+
+ptreps_selected <- list() #list of EO records for each selected NHA
+for (i in 1:length(eoid_list)){
+ptreps_selected[[i]] <- arc.select(ptreps, fields=c("EO_ID", "SNAME", "EO_DATA", "GEN_DESC","MGMT_COM","GENERL_COM"), where_clause=paste("EO_ID IN (", eoid_list[[i]], ")",sep="") )
+}
+
+# calculate the site significance rank based on the species present at the site #
+source(here::here("scripts","nha_ThreatsRecDatabase","2_loadSpeciesWeights.r"))
+
+sigrankspecieslist <- SD_speciesTable
+
+#remove species which are not relevant to site rankings--GNR, SNR, SH/Eo Rank H 
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) sigrankspecieslist[[x]][which(sigrankspecieslist[[x]]$GRANK!="GNR"&!is.na(sigrankspecieslist[[x]]$EORANK)),]) #remove EOs which are GNR
+
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) sigrankspecieslist[[x]][which(sigrankspecieslist[[x]]$SRANK!="SNR"&!is.na(sigrankspecieslist[[x]]$EORANK)),]) #remove EOs which are SNR
+
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) sigrankspecieslist[[x]][which(sigrankspecieslist[[x]]$SRANK!="SH"&!is.na(sigrankspecieslist[[x]]$EORANK)),]) #remove EOs which are SH
+
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) sigrankspecieslist[[x]][which(sigrankspecieslist[[x]]$EORANK!="H"),]) #remove EOs w/ an H quality rank
+
+#Merge rounded S, G, and EO ranks into individual species tables
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) merge(sigrankspecieslist[[x]], rounded_grank, by="GRANK"))
+
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) merge(sigrankspecieslist[[x]], rounded_srank, by="SRANK"))
+
+sigrankspecieslist <- lapply(seq_along(sigrankspecieslist), 
+                             function(x) merge(sigrankspecieslist[[x]], nha_EORANKweights, by="EORANK"))
+
+#Calculate rarity scores for each species within each table
+RarityScore <- function(x, matt) {
+  matt <- nha_gsrankMatrix
+  if (nrow(x) > 0) {
+    for(i in 1:nrow(x)) {
+      x$rarityscore[i] <- matt[x$GRANK_rounded[i],x$SRANK_rounded[i]] }}
+  else {
+    "NA"
+  }
+  x$rarityscore
+}
+
+res <- lapply(sigrankspecieslist, RarityScore) #calculate rarity score for each species table
+sigrankspecieslist2 <- Map(cbind, sigrankspecieslist, RarityScore=res) #bind rarity score into each species table
+
+#Calculate scores for each site, aggregating across all species and assign significance rank category. Skip any remaining NA values in the rarity scores      
+TotalScore  <- lapply(seq_along(sigrankspecieslist2), 
+                      function(x) sigrankspecieslist2[[x]]$RarityScore[!is.na(sigrankspecieslist2[[x]]$RarityScore)] * sigrankspecieslist2[[x]]$Weight) # calculate the total score for each species
+SummedTotalScore <- lapply(TotalScore, sum) 
+SummedTotalScore <- lapply(SummedTotalScore, as.numeric)
+
+SiteRank <- list() #create empty list object to write into
+
+for (i in seq_along(SummedTotalScore)) {
+  if(SummedTotalScore[[i]]==0|is.na(SummedTotalScore[[i]])){
+    SiteRank[[i]] <- "Local"
+  } else if(is.na(SummedTotalScore[[i]])){
+    SiteRank[[i]] <- "Local"
+  } else if(SummedTotalScore[[i]]>0 & SummedTotalScore[[i]]<=152) {
+    SiteRank[[i]] <- "State"
+  } else if(SummedTotalScore[i]>152 & SummedTotalScore[[i]]<=457) {
+    SiteRank[[i]] <- "Regional"
+  }  else if (SummedTotalScore[[i]]>457) {
+    SiteRank[[i]] <- "Global"
+  }
+}
+
